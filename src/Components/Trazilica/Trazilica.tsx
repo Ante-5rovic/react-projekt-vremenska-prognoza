1e684d51aadc52e7545f279bc7adbec25c042e1b
ante <ante.petrovic1002@gmail.com>
2023-11-14 17:23:41 +0100

    search bar sakrivanje

--- src/Components/Trazilica/Trazilica.tsx
+++ src/Components/Trazilica/Trazilica.tsx
@@ -1,7 +1,10 @@
-import React, { ChangeEvent, SyntheticEvent } from 'react'
+import React, { ChangeEvent, SyntheticEvent, useState } from 'react'
 import { IconButton } from "rsuite"; 
 import SearchIcon from '@rsuite/icons/Search';
 import './Trazilica.css';
+import {v4 as uuidv4} from 'uuid';
+
+
 
 type Props={
   onClick:(e:SyntheticEvent)=>void;
@@ -11,17 +14,19 @@ type Props={
 
 };
 
+
 const Trazilica:React.FC<Props> = ({onClick,search,handleChange,podatak}:Props):JSX.Element  => {
+  const [showSuggestions, setShowSuggestions] = useState(false);
   return (
     <div className='container_inputa'>
       <form className='input'>
-        <input className='input_box' type='input' placeholder='Unesi ime grada' value={search} onChange={(e)=>handleChange(e)} />
+        <input className='input_box' type='input' placeholder='Unesi ime grada' value={search} onChange={(e)=>handleChange(e)} onFocus={(e)=>{setShowSuggestions(true)}} onBlur={(e)=>{setShowSuggestions(false)}} />
         <IconButton className='search_button' icon={<SearchIcon />} type='button' onClick={(e)=> onClick(e)}/>
         
       </form>
-      <div className='dropdown'>
+      <div className='dropdown' style={{ display: showSuggestions ? 'flex' : 'none' }}>
           {podatak.length>0 && podatak.map((item)=>(
-            <div className='dropdown_row'>
+            <div className='dropdown_row' key={uuidv4()}>
               <div className='ime_grada'>{item.name}</div>
               <div className='zemlja'>{item.country}</div>
             </div>
