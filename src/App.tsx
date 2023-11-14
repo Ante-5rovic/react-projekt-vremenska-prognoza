3ca4f1be3cd8a9fbdb4cdf5bb43398ccc7f585b6
ante <ante.petrovic1002@gmail.com>
2023-11-14 06:33:33 +0100

    search prvi dio

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { ChangeEvent, SyntheticEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, SyntheticEvent, useEffect, useReducer, useState } from 'react';
 import './App.css';
 import Trazilica from './Components/Trazilica/Trazilica';
 //import Grad from './Components/Grad/Grad';
 import ListaGradova from './Components/ListaGradova/ListaGradova';
-import { geocodeLocation, searchGrad } from './api';
+import { geocodeLocation } from './api';
 
 
 function App() {
@@ -11,45 +11,74 @@ function App() {
   //console.log(geocodeLocation("ww"));
   const[search,setSearch]=useState<string>("Zagreb");
   const[podatak,setPodatak]=useState<ApiResponseGradovi[]>([]);
+  const[podatakZaPrikazUSearchz,setpodatakZaPrikazUSearchz]=useState<ApiResponseGradovi[]>([]);
   const[serverError,setServerError]=useState<string|null>(null);
   const [data, setData] = useState<ApiResponseGradovi>();
+  const [ignored, forceUpdate] = useReducer(x=>x+1,0);
+
+
 
   useEffect(() => {
-    const storedData = localStorage.getItem('myData');
-    if (storedData) {
-      setData(JSON.parse(storedData));
-    }
-  }, []);
-
-  const updateData = (newData:ApiResponseGradovi) => {
-    //setData(newData);
-    localStorage.setItem('myData', JSON.stringify(newData));
-  };
+    //console.log("ovo2")
+    //console.log(search)
+    handleDataForSearch();
+    console.log(podatakZaPrikazUSearchz)
+  }, [ignored]);
+  
 
 
 
 
-  const handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
+  
+
+    const  handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
       setSearch(e.target.value);
-      console.log(e);
+      //console.log(e.target.value+" lol?????");
+      forceUpdate();
+      //console.log(search+" lol222")
+      
+      
+      //dodaj sad elemente koji odgovaraj za prikaz na search baru
     };
+
+    const handleDataForSearch=async()=>{
+      if(search===""){
+        setpodatakZaPrikazUSearchz([]);
+      }else{
+        const podatakApia= await geocodeLocation(search);
+        if(podatakApia !==null && Array.isArray(podatakApia)){
+          setpodatakZaPrikazUSearchz(podatakApia);
+          //console.log(podatak);
+        }else if(podatakApia===null){
+          setServerError("Network Error");
+          //console.log("nema int");
+        }else{
+          //console.log('Error pri dohvatu podataka, vjv ne radi interface');
+        }
+      }
+      
+    };
+
+
+
     const onClick=async(e:SyntheticEvent)=>{
       const podatakApia= await geocodeLocation(search);
       if(podatakApia !==null && Array.isArray(podatakApia)){
         setPodatak(podatakApia);
         console.log(podatak);
       }else if(podatakApia===null){
-        setServerError("Network Error");
+        //setServerError("Network Error");
         //console.log("nema int");
       }else{
         console.log('Error pri dohvatu podataka, vjv ne radi interface');
       }
     };
+
   return (
     <div className='App'>
         <span className='heading'>Prognoza</span>
         {serverError && <h1>{serverError}</h1>}
-        <Trazilica onClick={onClick} search={search} handleChange={handleChange}/>
+        <Trazilica onClick={onClick} search={search} handleChange={handleChange} podatak={podatakZaPrikazUSearchz}/>
         <ListaGradova podatak={podatak}/>
     </div>
 
--- src/Components/Trazilica/Trazilica.tsx
+++ src/Components/Trazilica/Trazilica.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, SyntheticEvent, useState } from 'react'
+import React, { ChangeEvent, SyntheticEvent } from 'react'
 import { IconButton } from "rsuite"; 
 import SearchIcon from '@rsuite/icons/Search';
 import './Trazilica.css';
@@ -7,15 +7,30 @@ type Props={
   onClick:(e:SyntheticEvent)=>void;
   search:string|undefined;
   handleChange:(e:ChangeEvent<HTMLInputElement>)=>void;
+  podatak:ApiResponseGradovi[];
 
 };
 
-const Trazilica:React.FC<Props> = ({onClick,search,handleChange}:Props):JSX.Element  => {
+const Trazilica:React.FC<Props> = ({onClick,search,handleChange,podatak}:Props):JSX.Element  => {
   return (
-    <form className='input'>
+    <div className='container_inputa'>
+      <form className='input'>
         <input className='input_box' type='input' placeholder='Unesi ime grada' value={search} onChange={(e)=>handleChange(e)} />
         <IconButton className='search_button' icon={<SearchIcon />} type='button' onClick={(e)=> onClick(e)}/>
-    </form>
+        
+      </form>
+      <div className='dropdown'>
+          {podatak.length>0 && podatak.map((item)=>(
+            <div className='dropdown_row'>
+              <div className='ime_grada'>{item.name}</div>
+              <div className='zemlja'>{item.country}</div>
+            </div>
+          ))}
+          
+      </div>
+
+    </div>
+    
   )
 }
 
